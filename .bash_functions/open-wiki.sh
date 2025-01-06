071308d8e4554cf096afb8e426de75649f4db065
Guillermo Rebaza <grebaza@gmail.com>
2025-01-06 12:28:19 -0500

    use wiki dir by default

--- .bash_functions/open-wiki.sh
+++ .bash_functions/open-wiki.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 open-wiki() {
+  local dir="$1"
   local new_cwd
   local cwd_changed
 
-  if [[ -z "$(find . -maxdepth 1 -type f -regex '.*\.\(md\|txt\)' -print -quit)" ]]; then
+  if [[ -z "$dir" || -z "$(find "$dir" -maxdepth 1 -type f -regex '.*\.\(md\|txt\)' -print -quit)" ]]; then
     new_cwd="$HOME/wiki"
-    mkdir -p "$new_cwd" && pushd "$HOME/wiki" || return && cwd_changed=true
+    mkdir -p "$new_cwd" && pushd "$HOME/wiki" > /dev/null || return && cwd_changed=true
   fi
 
   # if [ -n "$WAYLAND_DISPLAY" ]; then
@@ -18,6 +19,6 @@ open-wiki() {
   if [[ -n "$file" ]]; then
     vim "$file"
   fi
-  [[ -n $cwd_changed ]] && popd || return
+  [[ -n $cwd_changed ]] && popd > /dev/null || return
 }
 alias ow=open-wiki
