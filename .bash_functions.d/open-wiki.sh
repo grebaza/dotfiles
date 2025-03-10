2e529c4b7836cc06118ddcceaf7bd56fe96147de
Guillermo Rebaza <grebaza@gmail.com>
2025-03-10 12:51:58 -0500

    add ow to local dir

--- .bash_functions.d/open-wiki.sh
+++ .bash_functions.d/open-wiki.sh
@@ -1,8 +1,12 @@
 openwiki() {
   local dir="${1:-.}"  # Default to cwd if no argument is provided
   local cwd_changed=false
+  local WIKI_DIR="$HOME/wiki"
 
-  [ -n "$dir" ] && pushd "$dir" &>/dev/null && cwd_changed=true
+  if ! find "$dir" -maxdepth 1 -type f \( -name "*.md" -o -name "*.txt" \) | grep -q '.'; then
+    dir="$WIKI_DIR"
+    pushd "$dir" &>/dev/null && cwd_changed=true
+  fi
 
   # Select file using ripgrep and rofi
   local file=$(rg --files --follow | rofi show-icons -dmenu -i -p "notes")
@@ -11,4 +15,4 @@ openwiki() {
   # Return to the previous directory only if we changed it
   $cwd_changed && popd &>/dev/null
 }
-alias ow='openwiki ~/wiki'
+alias ow=openwiki
