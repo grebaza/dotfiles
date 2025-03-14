d754a4bf6e79fa6f83bcfd265c35c772750711d7
Guillermo Rebaza <grebaza@gmail.com>
2025-03-14 11:57:33 -0500

    remove unnecessary fzf config

--- .bash_functions.d/fzf.sh
+++
@@ -1,5 +0,0 @@
-# shellcheck disable=SC2148
-# fzf ctrl-t for selected files and directorios
-# export FZF_CTRL_T_COMMAND='rg --files --hidden --follow'
-export FZF_CTRL_T_OPTS='--preview="batcat -n --color=always {}" --bind="ctrl-/:toggle-preview" --preview-window=:hidden'
-# alias fzfi='rg --files --hidden --follow --no-ignore-vcs -g "!{node_modules,.git}" | fzf'
